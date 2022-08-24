9935174e9f67e521ddcada71e4a5fd3125bc708d
theocode12 <vchidubem9@gmail.com>
2022-08-24 03:01:36 +0100

    Binary Trees

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,6 +9,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth  = 0;
 
+	if (!tree)
+		return (0);
 	if (!(tree->parent))
 		return (0);
 	depth = binary_tree_depth(tree->parent) + 1;
